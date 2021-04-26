95965a6a970be2d0bc098ad92ad3adc605421fed
Sagar kumar jha <jha8768@gmail.com>
2021-04-27 03:17:44 +0530

    Adding items in cart using sessions

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
-console.log("hello from resources");
+import axios from 'axios';
+
+let addTocart=document.querySelectorAll(".add-to-cart");
+let cartCounter=document.getElementById("cart-counter")
+
+function updateCart(pizza){
+    axios.post("/update-cart",pizza).then((res)=>{
+        // console.log(res);
+        cartCounter.innerText=res.data.totalQty;
+    });
+}
+
+addTocart.forEach((btn)=>{
+    btn.addEventListener("click",(e)=>{
+        let pizza=JSON.parse(btn.dataset.pizza);
+        updateCart(pizza);
+    });
+});
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const ejs = require("ejs");
 const expressLayout = require("express-ejs-layouts");
 const path = require("path");
-const mongoose = require("mongoose");
-
 const PORT = process.env.PORT || 3300;
+const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("express-flash");
+const MongoDbStore = require("connect-mongo")(session);
 
 //mongoDB connection
 const url = "mongodb://localhost/pizzalo";
@@ -24,8 +27,34 @@ connection
 		console.log("connection failed....");
 	});
 
+//session store
+let mongoStore = new MongoDbStore({
+	mongooseConnection: connection,
+	collection: "sessions",
+});
+
+//Session config
+app.use(
+	session({
+		secret: process.env.COOKIE_SECRET,
+		resave: false,
+		saveUninitialized: false,
+		store: mongoStore,
+		cookie: { maxAge: 1000 * 60 * 60 * 24 },
+	})
+);
+
+app.use(flash());
+
 //assets
 app.use(express.static("public"));
+app.use(express.json());
+
+//global middlewares
+app.use((req,res,next)=>{
+	res.locals.session=req.session;
+	next();
+})
 
 //set template emgine
 app.use(expressLayout);
