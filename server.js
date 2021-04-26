65f15c75375b370619fe4d2145823c1f47b300e4
Sagar kumar jha <jha8768@gmail.com>
2021-04-27 01:41:11 +0530

    Database connection and dynamic homepage

--- server.js
+++ server.js
@@ -3,8 +3,26 @@ const app = express();
 const ejs = require("ejs");
 const expressLayout = require("express-ejs-layouts");
 const path = require("path");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3300;
+
+//mongoDB connection
+const url = "mongodb://localhost/pizzalo";
+mongoose.connect(url, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+	useFindAndModify: true,
+});
+const connection = mongoose.connection;
+connection
+	.once("open", () => {
+		console.log("Database Connected....");
+	})
+	.catch((err) => {
+		console.log("connection failed....");
+	});
 
 //assets
 app.use(express.static("public"));
@@ -14,23 +32,8 @@ app.use(expressLayout);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/resources/views"));
 
-
-
-app.get("/", (req, res) => {
-	res.render("home");
-});
-
-app.get("/cart",(req,res)=>{
-	res.render("customers/cart");
-});
-
-app.get("/login",(req,res)=>{
-	res.render("auth/login");
-});
-
-app.get("/register",(req,res)=>{
-	res.render("auth/register");
-});
+//routes
+require("./routes/web")(app);
 
 app.listen(PORT, () => {
 	console.log(`Server is listening on port ${PORT}`);
