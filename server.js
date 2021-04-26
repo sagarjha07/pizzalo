a868b7a3a39f4795028116a86ff8ffbec8f6b344
Sagar kumar jha <jha8768@gmail.com>
2021-04-26 19:40:07 +0530

    login and register page added

--- server.js
+++ server.js
@@ -14,12 +14,22 @@ app.use(expressLayout);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/resources/views"));
 
+
+
 app.get("/", (req, res) => {
 	res.render("home");
 });
 
 app.get("/cart",(req,res)=>{
 	res.render("customers/cart");
+});
+
+app.get("/login",(req,res)=>{
+	res.render("auth/login");
+});
+
+app.get("/register",(req,res)=>{
+	res.render("auth/register");
 });
 
 app.listen(PORT, () => {
