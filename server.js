e372273934417aa872a6d8576fe148ba5b6ffa4a
Sagar kumar jha <jha8768@gmail.com>
2021-04-26 19:05:09 +0530

    cart page added

--- server.js
+++ server.js
@@ -10,12 +10,16 @@ const PORT = process.env.PORT || 3300;
 app.use(express.static("public"));
 
 //set template emgine
-// app.use(expressLayout);
+app.use(expressLayout);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/resources/views"));
 
 app.get("/", (req, res) => {
 	res.render("home");
+});
+
+app.get("/cart",(req,res)=>{
+	res.render("customers/cart");
 });
 
 app.listen(PORT, () => {
